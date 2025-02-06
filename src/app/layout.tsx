7d0d5da571550775899732392702f226487de3e5
daniel <daniel.pinedamo@amigo.edu.co>
2025-02-06 16:30:44 -0500

    feat: :sparkles: importar react hot toast y prime react
    importar react hot toast y prime react
    
    importar react hot toast y prime react

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { PrimeReactProvider } from "primereact/api";
+import { Toaster } from "react-hot-toast";
 import "../scss/index.scss";
 
 const geistSans = localFont({
@@ -28,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Toaster position="top-right" reverseOrder={true} />
+
+        <PrimeReactProvider>{children}</PrimeReactProvider>
       </body>
     </html>
   );
