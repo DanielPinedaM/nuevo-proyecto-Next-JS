c68b853f22a41d36ed68b2858306d9fa527c9814
daniel <daniel.pinedamo@amigo.edu.co>
2025-05-20 19:35:24 -0500

    feat: :sparkles: crear componente para listar tabla dinamica
    crear componente para listar tabla dinamica
    
    crear componente para listar tabla dinamica

---
+++ src/components/ListDynamicTable.tsx
@@ -0,0 +1,58 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { DataTable } from "primereact/datatable";
+import { rowsPerPageOptions, titleCase } from "@/utils/func/general";
+import { Column } from "primereact/column";
+import visibleRows from "@/types/constant/const-visible-rows";
+
+/**
+Muestra una tabla de forma dinamica,
+sin importar q informacion tenga la tabla
+
+
+la tabla NO esta anidada - tableDataDynamic es un array de objetos NO anidado */
+export default function ListDynamicTable({ tableDataDynamic }: { tableDataDynamic: Record<string, any>[] }) {
+  const Table = () => {
+    const columns: string[] = Object?.keys(tableDataDynamic[0])?.toSorted((a, b) => a?.localeCompare(b, "es-ES"));
+
+    return (
+      <DataTable
+        value={tableDataDynamic}
+        paginator={tableDataDynamic.length > visibleRows}
+        rows={visibleRows}
+        totalRecords={tableDataDynamic.length}
+        rowsPerPageOptions={rowsPerPageOptions(tableDataDynamic.length, visibleRows)}
+      >
+        {Array.isArray(columns) && columns?.length &&
+          columns.map((item) => (
+            <Column
+              key={item}
+              field={item}
+              header={titleCase(item.replaceAll("_", " ").replaceAll("-", " "))}
+              body={(data) => {
+                const value: any = data[item];
+
+                if (value === null || String(value).trim().toLowerCase() === "null") {
+                  return <span>null</span>;
+                } else if (typeof value === "object") {
+                    return <span>{JSON.stringify(value)}</span>
+                } else {
+                  return <span>{String(value)}</span>;
+                }
+              }}
+            />
+          ))
+        }
+      </DataTable>
+    );
+  };
+
+  return (
+    <>
+      {Array.isArray(tableDataDynamic) && tableDataDynamic?.length ? (
+        <Table />
+      ) : (
+        <p className="text-center">No hay datos</p>
+      )}
+    </>
+  );
+}
---
+++ src/types/constant/const-visible-rows.ts
@@ -0,0 +1,4 @@
+/**
+prime NG - numero de filas q inicialmente muestra el paginador de las tablas */
+const visibleRows: number = 10;
+export default visibleRows;
