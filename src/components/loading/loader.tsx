630c3582527e7f90b7d822ebf6a6c6219f43e8a6
daniel <daniel.pinedamo@amigo.edu.co>
2025-05-20 19:20:59 -0500

    feat: :recycle: refactorizar loader
    refactorizar loader
    
    refactorizar loader

--- src/components/loading/loader.tsx
+++ src/components/loading/loader.tsx
@@ -28,11 +28,13 @@ export default function Loader() {
 
   return (
     <>
-      {isLoading ? (
+    {
+      isLoading && (
         <div className="bg-opaque-white w-full h-full flex justify-center items-center fixed z-[999999] cursor-wait">
           <RiLoader4Fill className="animate-spin text-blue-500 text-7xl" />
         </div>
-      ) : null}
+      )
+    }
     </>
   );
 }
