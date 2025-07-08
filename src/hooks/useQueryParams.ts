380ec5464ce34d13c3bf2780755ac88517738ab7
daniel <daniel.pinedamo@amigo.edu.co>
2025-07-07 19:44:38 -0500

    feat: :hammer: mejorar useQueryParams.ts
    mejorar useQueryParams.ts
    
    mejorar useQueryParams.ts

--- src/hooks/useQueryParams.ts
+++ src/hooks/useQueryParams.ts
@@ -1,13 +1,13 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-'use client';
-import { useRouter, useSearchParams, usePathname } from 'next/navigation';
-import { useCallback } from 'react';
-import { useNavigationLoaderStore } from '@/store/loader/navigationLoaderStore';
-import { forceConvertToString, literalObjectLength } from '@/utils/func/dataType';
+"use client";
+import { useRouter, useSearchParams, usePathname } from "next/navigation";
+import { useCallback } from "react";
+import { useNavigationLoaderStore } from "@/config/stores/loaderStore";
+import { forceConvertToString, literalObjectLength } from "@/utils/func/data-type.utils";
 
 interface IOptionsSetQueryParams {
   replaceAll?: boolean;
   showLoader?: boolean;
+  customPathname?: string;
 }
 
 export const useQueryParams = () => {
@@ -22,21 +22,24 @@ export const useQueryParams = () => {
    * http://nextjs.org/docs/app/api-reference/functions/use-search-params#updating-searchparams
    *
    * @param {Object} [paramsObj] — objeto literal con query params a actualizar
+   * @param {Object} [customPathname] — ruta personalizada a la cual se le setea los query params, por defecto es la ruta actual
    * @param {boolean} [options.replaceAll] — true = actualizar los nuevos valores cuando las keys de paramsObj existen en los query params, false = reemplazar POR COMPLETO por nuevos query params
    * @param {boolean} [options.showLoader] — true = MOSTRAR icono de cargando, false = OCULTAR icono de cargando */
   const setQueryParams = useCallback(
     (paramsObj: Record<string, any>, options?: IOptionsSetQueryParams): void => {
       if (literalObjectLength(paramsObj) <= 0) return;
 
-      const { replaceAll = false, showLoader = true } = options ?? {};
+      const {
+        replaceAll = false,
+        showLoader = true,
+        customPathname = null,
+      } = options ?? {};
 
       // uso window.location.search porque useSearchParams() NO actualiza la variable oldUrl con el ultimo query params
-      const currentParams: URLSearchParams = new URLSearchParams(window.location.search);
+      const currentParams = new URLSearchParams(window.location.search);
 
       // url ANTES de actualizar query params
-      const oldUrl: string = currentParams.toString()
-        ? `${pathname}?${currentParams.toString()}`
-        : pathname;
+      const oldUrl: string = currentParams.toString() ? `${pathname}?${currentParams.toString()}` : pathname;
 
       const params: URLSearchParams = replaceAll
         ? new URLSearchParams() // se parte de cero, sin query actual
@@ -47,7 +50,7 @@ export const useQueryParams = () => {
       });
 
       // url DESPUES de actualizar query params
-      const newUrl: string = `${pathname}?${params.toString()}`;
+      const newUrl: string = `${customPathname ?? pathname}?${params.toString()}`;
 
       // NO actualizar la URL cuando los query params no han cambiado
       if (oldUrl !== newUrl) {
