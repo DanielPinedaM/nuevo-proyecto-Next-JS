8759f9dcc8f52a0152dc18e7119e812b68bb8e21
daniel <daniel.pinedamo@amigo.edu.co>
2025-07-10 23:02:06 -0500

    feat: :lipstick: mejorar skeleton menu
    mejorar skeleton menu
    
    mejorar skeleton menu

--- src/components/loading/skeleton/SkeletonMenu.tsx
+++ src/components/loading/skeleton/SkeletonMenu.tsx
@@ -9,7 +9,7 @@ export default function SkeletonMenu({ length }: ISkeletonMenu) {
   const items: unknown[] = Array.from({ length: skeletonLength });
 
   return (
-    <ul className='rounded-md py-4 px-2 bg-gray-200 w-25 flex-none flex flex-col justify-between gap-y-4 h-full'>
+    <ul className='cursor-wait rounded-md py-4 px-2 bg-gray-200 w-25 flex-none flex flex-col justify-between gap-y-4 h-full'>
       {items.map((_, i: number) => (
         <li key={i}>
           <div className='animate-pulse bg-gray-400 rounded h-2' />
