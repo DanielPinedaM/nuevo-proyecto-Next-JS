5f8681fb8838ddd10970046f041e17c681b65b25
DanielPinedaM <daniel.pinedamo@amigo.edu.co>
2025-08-19 15:41:51 -0500

    mejorar luxon.utils.ts

--- src/utils/func/luxon.utils.ts
+++ src/utils/func/luxon.utils.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Nullable } from 'primereact/ts-helpers';
-import { DateTime } from 'luxon';
+import { Nullable } from "primereact/ts-helpers";
+import { DateTime } from "luxon";
 
 /**
 Eliminar espacio en blanco reemplazando:
@@ -10,45 +10,24 @@ const replaceAmPm = (date: Date | string | any): string | any => {
   if (!date) return date;
 
   return date
-    .replace(/p\.(\s| )m/gi, 'p.m')
-    .replace(/a\.(\s| )m/gi, 'a.m')
-    .replace(/\.$/, '');
+    .replace(/p\.(\s| )m/gi, "p.m")
+    .replace(/a\.(\s| )m/gi, "a.m")
+    .replace(/\.$/, "");
 };
 
 /**
-formato de hora */
-export const formatHour = (
-  date: Date | string | Nullable<Date> | any,
-  format: string = 'hh:mm:ss a'
-): string | any => {
-  let dateTime: DateTime;
-
-  if (date instanceof Date) {
-    dateTime = DateTime.fromJSDate(date);
-  } else if (typeof date === 'string' && String(date)?.trim() !== '') {
-    dateTime = DateTime.fromISO(date);
-  } else {
-    return date;
-  }
-
-  if (!dateTime.isValid) return date;
-
-  const finalDate: string = dateTime.setLocale('es').toFormat(format);
-
-  return replaceAmPm(finalDate);
-};
-
-/**
-formato de fecha */
+formato de fecha y/o hora con formato personalizado */
 export const formatDate = (
   date: Date | string | Nullable<Date> | DateTime,
-  format: string = 'd-LLL-yyyy'
+  format: string = "d-LLL-yyyy"
 ): string | any => {
   let dateTime: DateTime;
 
-  if (date instanceof Date) {
+  if (date instanceof DateTime) {
+    dateTime = date;
+  } else if (date instanceof Date) {
     dateTime = DateTime.fromJSDate(date);
-  } else if (typeof date === 'string' && String(date)?.trim() !== '') {
+  } else if (typeof date === "string" && String(date)?.trim() !== "") {
     dateTime = DateTime.fromISO(date);
   } else {
     return date;
@@ -56,43 +35,16 @@ export const formatDate = (
 
   if (!dateTime.isValid) return date;
 
-  const finalDate: string = dateTime.setLocale('es').toFormat(format);
-
-  return replaceAmPm(finalDate);
+  return replaceAmPm(dateTime.setLocale("es").toFormat(format));
 };
 
 /**
-formato de fecha y hora con a.m y p.m */
-export const dateAndTimeFormat = (date: Date | string | Nullable<Date>): string | any => {
-  let dateTime: DateTime;
-
-  if (date instanceof Date) {
-    dateTime = DateTime.fromJSDate(date);
-  } else if (typeof date === 'string' && String(date)?.trim() !== '') {
-    dateTime = DateTime.fromISO(date);
-  } else {
-    return date;
-  }
-
-  if (!dateTime.isValid) return date;
-
-  const finalDate: string = dateTime
-    .setLocale('es')
-    .toFormat('d-LLL-yyyy hh:mm a')
-    .replace(/\.$/, '')
-    .replace('p. m', 'p.m')
-    .replace('a. m', 'a. m');
-
-  return replaceAmPm(finalDate);
-};
-
-/**
-fecha y hora actual con a.m y p.m  */
-export const currentDateAndTime = (): string => {
+fecha y hora actual con formato de hora personalizado */
+export const currentDateAndTime = (format: string = "d-LLL-yyyy hh:mm:ss a"): string => {
   const finalDate: string = DateTime.now()
-    .setLocale('es')
-    .toFormat('d-LLL-yyyy hh:mm:ss a')
-    .replace(/\.$/, '');
+    .setLocale("es")
+    .toFormat(format)
+    .replace(/\.$/, "");
 
   return replaceAmPm(finalDate);
 };
