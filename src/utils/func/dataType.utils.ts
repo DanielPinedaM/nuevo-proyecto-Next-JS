1c984a9d7831a43d12896d601004149e1c440c1f
DanielPinedaM <daniel.pinedamo@amigo.edu.co>
2025-08-27 20:27:31 -0500

    feat: :recycle: mejorar funcion para Normalizar string
    mejorar funcion para Normalizar string
    
    mejorar funcion para Normalizar string

--- src/utils/func/dataType.utils.ts
+++ src/utils/func/dataType.utils.ts
@@ -38,54 +38,86 @@ export const isNumber = (variable: number | any): boolean => {
 * [1, 2, 3]           devuelve   [1, 2, 3]
 * 
 * @param {string|any} string — valor a normalizar. Si no es string o está vacío, se devuelve tal cual
-* @param {Object} [options] — opciones de normalización
-* @param {boolean} [options.clearSpecialCharacters] — true = BORRAR caracteres especiales,  false = CONSERVAR caracteres especiales
-* @param {boolean} [options.enyeWithN] — true = REEMPLAZAR "ñ" y "Ñ" por "n", false = CONSERVAR letra "ñ"
-* @param {boolean} [options.clearNumbers] — true = BORRAR numeros, false = CONSERVAR numeros
-* @param {boolean} [options.upperCase] — true = convertir texto a MAYUSCULA, false = convertir texto a minuscula
-* @returns {string|any} — la cadena normalizada o el valor original si no es string */
+
+* @param {Object} [options]                          — opciones de normalización
+* @param {boolean} [options.clearAccents ]           — true = borrar las tildes, false = NO borrar las tildes
+* @param {boolean} [options.clearSpecialCharacters]  — true = BORRAR caracteres especiales,  false = CONSERVAR caracteres especiales
+* @param {boolean} [options.enyeWithN]               — true = REEMPLAZAR "ñ" y "Ñ" por "n", false = CONSERVAR letra "ñ"
+* @param {boolean} [options.clearNumbers]            — true = BORRAR numeros, false = CONSERVAR numeros
+* @param {boolean} [options.upperCase]               — true = convertir texto a MAYUSCULA, false = convertir texto a minuscula
+* @param {boolean} [options.trimType]                — controla como borrar los espacios en blanco: "trim" borrar espacio en blanco al PRINCIPIO Y FINAL, "trimStart" borrar espacio en blanco al PRINCIPIO, "trimEnd" borrar espacio en blanco al FINAL, null NO borrar espacios en blanco
+
+* @returns {string|any}                              — la cadena normalizada o el valor original si no es string */
+interface IOptionsNormalizeStr {
+    clearAccents?: boolean;
+    clearSpecialCharacters?: boolean; 
+    enyeWithN?: boolean; 
+    clearNumbers?: boolean;
+    upperCase?: boolean;
+    trimType?: "trim" | "trimStart" | "trimEnd" | null
+} 
 export const normalizeStr = (
   string: string | any,
-  options?: { clearSpecialCharacters?: boolean; enyeWithN?: boolean; clearNumbers?: boolean, upperCase?: boolean }
+  options?: IOptionsNormalizeStr
 ): string | any => {
   if (!isString(string)) return string;
   if (String(string).trim() === '') return '';
 
-     const {
+       const {
+        clearAccents = true,
         clearSpecialCharacters = false,
         enyeWithN = false,
         clearNumbers = false,
-        upperCase = false
+        upperCase = false,
+        trimType = "trim",
      } = options ?? {};
 
-     let newString: string = string.toLowerCase()                                    // convertir a minuscula
-                                   .normalize("NFD")                                 // hacer q funcionen las expresiones regulares
-                                   .replaceAll(/[\u0300-\u0302\u0304-\u036f]/g, "")  // eliminar acentos (todos menos U+0303)
-                                   .normalize("NFC")                                 // conservar la "ñ" "Ñ"
-                                   
+     let newString: string = string.toLowerCase()                              // convertir a minuscula
+                                   .normalize("NFD")                           // hacer q funcionen las expresiones regulares 
+     if (clearAccents) {
+        newString = newString.replaceAll(/[\u0300-\u0302\u0304-\u036f]/g, "")  // eliminar acentos (todos menos U+0303)
+     }
+
+     newString = newString.normalize("NFC")                                    // conservar la "ñ" "Ñ"
 
       if (enyeWithN) {
-        newString = newString.replaceAll(/ñ/g, 'n');                                 // reemplazar ñ minúscula por n
+        newString = newString.replaceAll(/ñ/g, 'n');                           // reemplazar ñ minúscula por n
       }
 
       if (clearSpecialCharacters) {
-        newString = newString.replaceAll(/[^a-zA-Z0-9 ñÑ]/g, '');                    // borrar caracteres especiales
+        newString = newString.replaceAll(/[^a-zA-Z0-9 ñÑ]/g, '');              // borrar caracteres especiales
       }
 
       if (clearNumbers) {
-       newString = newString.replaceAll(/\d+/g, '');                                 // borrar todos los numeros 0123456789
-      }
-      
-      // esto TIENE q estar al final de la funcion
-      if (upperCase) {
-        newString = newString.toLocaleUpperCase("es-ES");
+       newString = newString.replaceAll(/\d+/g, '');                           // borrar todos los numeros 0123456789
       }
 
-      newString = newString.trim()                                                   // borrar espacio en blanco al principio y final
-                           .replaceAll(/\s+/g, ' ')                                  // reemplazar múltiples espacios en blanco '   ' por un solo espacio en blanco ' ';
-      
-      return newString;
+      /*
+      *********************************************
+      * esto TIENE q estar al final de la funcion *
+      ********************************************* */
+
+      if (upperCase) {
+        newString = newString.toLocaleUpperCase("es-ES");                      // convertir a MAYUSCULA
+      }
+
+            if (trimType === "trim") {
+        newString = newString.trim();                                          // borrar espacio en blanco al PRINCIPIO Y FINAL
+      }
+
+      if (trimType === "trimStart") {
+        newString = newString.trimStart();                                     // borrar espacio en blanco al PRINCIPIO
+      }
+
+      if (trimType === "trimEnd") {
+        newString = newString.trimEnd();                                       // borrar espacio en blanco al FINAL
+      }
+
+      newString = newString.replaceAll(/\s+/g, ' ')                            // reemplazar múltiples espacios en blanco '   ' por un solo espacio en blanco ' '
+
+      return newString;
 };
+
 
 /**
 SI es posible convierte a NUMERO,
