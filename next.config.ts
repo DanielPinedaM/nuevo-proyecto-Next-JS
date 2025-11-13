588a260ced7fbfc5a7c1bc0766332c8f7ef80001
DanielPinedaM <daniel.pinedamo@amigo.edu.co>
2025-11-13 12:59:43 -0500

    feat: :sparkles: en produccion activar SourceMaps, en develop desactivar SourceMaps
    en produccion activar SourceMaps, en develop desactivar SourceMaps
    
    en produccion activar SourceMaps, en develop desactivar SourceMaps

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  productionBrowserSourceMaps: process.env.NEXT_PUBLIC_NODE_ENV !== "production",
 };
 
 export default nextConfig;
