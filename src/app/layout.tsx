5e763b91fca5a2ae9ad638e086306cfb34227e19
DanielPinedaM <daniel.pinedamo@amigo.edu.co>
2026-06-04 17:41:32 -0500

    style: :lipstick: mejorar orden de estilos globales de sass

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import '../styles/index.scss';
+import '../styles/main.scss';
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import { PrimeReactProvider } from 'primereact/api';
