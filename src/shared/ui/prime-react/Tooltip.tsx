2d290b50d29727abd0f8499bc5a26724e5225001
DanielPinedaM <daniel.pinedamo@amigo.edu.co>
2026-06-20 13:52:42 -0500

    refactor: :recycle: corregir tipos de tooltip

--- src/shared/ui/prime-react/Tooltip.tsx
+++ src/shared/ui/prime-react/Tooltip.tsx
@@ -1,24 +1,21 @@
 import { Tooltip as PrimeReactTooltip, TooltipProps } from "primereact/tooltip";
 
-interface ITooltipProps {
+interface ITooltipProps extends TooltipProps {
   // nombre de la clase a la cual esta asociada el tooltip
   target: string;
-
-  // texto del tooltip
-  content?: string;
-
-  // lugar donde se ubica el tooltip, undefined es automatico
-  position?: TooltipProps["position"];
-}
+}{}
 
 export default function Tooltip({
   target,
   content = "",
   position = undefined,
+  className = "hidden xl:inline-block",
+  ...rest
 }: ITooltipProps) {
   return (
     <PrimeReactTooltip
-      className='hidden xl:inline-block'
+      {...rest}
+      className={className}
       target={`.${target}`}
       content={content ? content : ""}
       position={typeof position === "string" ? position : undefined}
